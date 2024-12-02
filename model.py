b3c0db9c735deb45517f55e8223e9c99f20487e5
Gabriel Pirlogeanu <53811655+gabitza-tech@users.noreply.github.com>
2024-12-02 20:27:28 +0200

    Update model.py

--- model.py
+++ model.py
@@ -10,7 +10,7 @@ This model is modified and combined based on the following three projects:
 import math, torch, torchaudio
 import torch.nn as nn
 import torch.nn.functional as F
-from ecapa.melspec import MelSpectrogramNoTorchAudio
+from melspec import MelSpectrogramNoTorchAudio
 
 class SEModule(nn.Module):
     def __init__(self, channels, bottleneck=128):
